79399a7e231ccf2357f4cca610ea480d157766bd
jamie <jamie.levine.phone@gmail.com>
2020-08-28 13:07:13 -0400

    Adding High level keywords from files and insert into scripts
    its crufty but it works and i want to move on to other areas without rebase hell, so i am good with it

--- src/locator/classifier.js
+++ src/locator/classifier.js
@@ -1,84 +1,84 @@
 
 const classifier = { 
- classify (element) {
-  let hash = null;
-  const tag = element.tagName.toLowerCase();
-  if (tag === 'input') {
-    switch (element.type) {
-      case 'password':
-        hash = { type: 'text', value: '***' };
-        break;
-      case 'radio':
-        hash = { type: 'radio', value: element.value };
-        break;
-      case 'checkbox':
-        hash = { type: 'checkbox', value: element.checked };
-        break;
-      case 'file':
-        hash = { type: 'file', value: element.value };
-        break;
-      case 'email':
-      case 'tel':
-      case 'url':
-      case 'number':
-      case 'search':
-      case 'text':
-      case 'date':
-      case 'datetime-local':
-      case 'week':
-      case 'month':
-      case 'color':
-        hash = { type: 'text', value: element.value };
-        break;
-      case 'submit':
-      case 'image':
-      case 'range':
-      case 'reset':
-        hash = { type: element.type };
-        break;
-      case 'hidden':
-      default:
-        break;
-    }
-  } else if (tag === 'textarea') {
-    hash = { type: 'text', value: element.value };
-  } else if (tag === 'select') {
-    hash = { type: 'select', value: element.value };
-  } else if (tag === 'a') {
-    hash = { type: 'a', value: element.href };
-  } else if (element.innerText !== null && element.innerText !== "" ) {
-    hash = { type: 'containsText', value: element.innerText.substring(0,15)  };
-  } else if (element.parentNode.textContent !== null && element.parentNode.textContent !== "") {
-    // if there is text one parent up then down
-    let pTag = this._searchForText(element.parentNode, element.parentNode.textContent)
-
-    hash = { type: 'parentContainsText', value: element.parentNode.textContent.substring(0,15), parentTag: pTag  };
-  } else if (element.parentNode.parentNode.textContent !== null && element.parentNode.parentNode.textContent !== "") {
-        // if there is text two parent up then down
-        // this should be dynamic to find n parents up
-    let pTag = this._searchForText(element.parentNode.parentNode, element.parentNode.parentNode.textContent)
-    hash = { type: 'parentParentContainsText', value: element.parentNode.parentNode.textContent.substring(0,15), parentTag: pTag  };
-  }
-  return hash;
-},
-
-
-// serach for text in the child of the passed node
- _searchForText(element, elementText) {
-
-  const aTags = element;
-  const searchText = elementText;
-  let found;
-
-for (let i = 0; i < aTags.children.length; i++) {
-  // check if children have textContent
-  if (aTags.children[i].textContent == searchText.toString()) {
-    found = aTags.children[i];
-    break;
-  }
-}
-  return found
-}
-
-};
-if (typeof exports !== 'undefined') module.exports.classifier = classifier;
+  classify (element) {
+   let hash = null;
+   const tag = element.tagName.toLowerCase();
+   if (tag === 'input') {
+     switch (element.type) {
+       case 'password':
+         hash = { type: 'text', value: '***' };
+         break;
+       case 'radio':
+         hash = { type: 'radio', value: element.value };
+         break;
+       case 'checkbox':
+         hash = { type: 'checkbox', value: element.checked };
+         break;
+       case 'file':
+         hash = { type: 'file', value: element.value };
+         break;
+       case 'email':
+       case 'tel':
+       case 'url':
+       case 'number':
+       case 'search':
+       case 'text':
+       case 'date':
+       case 'datetime-local':
+       case 'week':
+       case 'month':
+       case 'color':
+         hash = { type: 'text', value: element.value };
+         break;
+       case 'submit':
+       case 'image':
+       case 'range':
+       case 'reset':
+         hash = { type: element.type };
+         break;
+       case 'hidden':
+       default:
+         break;
+     }
+   } else if (tag === 'textarea') {
+     hash = { type: 'text', value: element.value };
+   } else if (tag === 'select') {
+     hash = { type: 'select', value: element.value };
+   } else if (tag === 'a') {
+     hash = { type: 'a', value: element.href };
+   } else if (element.innerText !== null && element.innerText !== "" ) {
+     hash = { type: 'containsText', value: element.innerText.substring(0,15)  };
+   } else if (element.parentNode.textContent !== null && element.parentNode.textContent !== "") {
+     // if there is text one parent up then down
+     let pTag = this._searchForText(element.parentNode, element.parentNode.textContent)
+ 
+     hash = { type: 'parentContainsText', value: element.parentNode.textContent.substring(0,15), parentTag: pTag  };
+   } else if (element.parentNode.parentNode.textContent !== null && element.parentNode.parentNode.textContent !== "") {
+         // if there is text two parent up then down
+         // this should be dynamic to find n parents up
+     let pTag = this._searchForText(element.parentNode.parentNode, element.parentNode.parentNode.textContent)
+     hash = { type: 'parentParentContainsText', value: element.parentNode.parentNode.textContent.substring(0,15), parentTag: pTag  };
+   }
+   return hash;
+ },
+ 
+ 
+ // serach for text in the child of the passed node
+  _searchForText(element, elementText) {
+ 
+   const aTags = element;
+   const searchText = elementText;
+   let found;
+ 
+ for (let i = 0; i < aTags.children.length; i++) {
+   // check if children have textContent
+   if (aTags.children[i].textContent == searchText.toString()) {
+     found = aTags.children[i];
+     break;
+   }
+ }
+   return found
+ }
+ 
+ };
+ if (typeof exports !== 'undefined') module.exports.classifier = classifier;
--- src/popup.js
+++ src/popup.js
@@ -1,27 +1,30 @@
 /* global document $ chrome ClipboardJS */
 const debug = false;
-const gaAccount = 'UA-88380525-1';
-const version = '0.3.0';
+const gaAccount = "UA-88380525-1";
+const version = "0.3.0";
+const once = {
+  once : true
+};
 
 const host = chrome;
 const storage = host.storage.local;
 
 /*eslint-disable */
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', gaAccount]);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
+_gaq.push(["_setAccount", gaAccount]);
+_gaq.push(["_trackPageview"]);
+(function () {
+  var ga = document.createElement("script");
+  ga.type = "text/javascript";
   ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
+  ga.src = "https://ssl.google-analytics.com/ga.js";
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(ga, s);
 })();
 /* eslint-enable */
 
 function logger(data) {
-  if (debug) document.getElementById('textarea-log').value = data;
+  if (debug) document.getElementById("textarea-log").value = data;
 }
 
 function analytics(data) {
@@ -33,177 +36,376 @@ function analytics(data) {
   }
 }
 
-const clipboard = new ClipboardJS('#copy');
+const clipboard = new ClipboardJS("#copy");
 
 const copyStatus = (className) => {
-  $('#copy').addClass(className);
-  setTimeout(() => { $('#copy').removeClass(className); }, 3000);
+  $("#copy").addClass(className);
+  setTimeout(() => {
+    $("#copy").removeClass(className);
+  }, 3000);
 };
 
-clipboard.on('success', (e) => {
-  copyStatus('copy-ok');
-  analytics(['_trackEvent', 'copy', 'ok']);
+clipboard.on("success", (e) => {
+  copyStatus("copy-ok");
+  analytics(["_trackEvent", "copy", "ok"]);
 
   e.clearSelection();
 });
 
-clipboard.on('error', (e) => {
-  copyStatus('copy-fail');
-  analytics(['_trackEvent', 'copy', 'nok']);
+clipboard.on("error", (e) => {
+  copyStatus("copy-fail");
+  analytics(["_trackEvent", "copy", "nok"]);
   /* eslint-disable no-console */
-  console.error('Action:', e.action);
-  console.error('Trigger:', e.trigger);
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
   /* eslint-enable no-console */
 });
 
 function display(message) {
   if (message && message.message) {
-    const field = document.querySelector('#textarea-script');
-    field.value = message.message || '';
+    const field = document.querySelector("#textarea-script");
+    field.value = message.message || "";
   }
 }
 
 function show(array, visible) {
   array.forEach((id) => {
     const element = document.getElementById(id);
-    visible ? element.classList.remove('hidden') : element.classList.add('hidden');
+    visible
+      ? element.classList.remove("hidden")
+      : element.classList.add("hidden");
   });
 }
 
 function enable(array, isEnabled) {
   array.forEach((id) => {
     const element = document.getElementById(id);
-    isEnabled ? element.classList.remove('disabled') : element.classList.add('disabled');
+    isEnabled
+      ? element.classList.remove("disabled")
+      : element.classList.add("disabled");
   });
 }
 
 function toggle(e) {
   logger(e.target.id);
-  if (e.target.id === 'record') {
-    show(['stop', 'pause'], true);
-    show(['record', 'resume', 'scan'], false);
-    enable(['settings-panel'], false);
 
-    $('#sortable').sortable('disable');
-  } else if (e.target.id === 'pause') {
-    show(['resume', 'stop'], true);
-    show(['record', 'scan', 'pause'], false);
-    enable(['settings-panel'], false);
+  if (e.target.id === "record") {
+    show(["stop", "pause", "pom"], true);
+    show(["record", "resume", "scan"], false);
+    enable(["settings-panel"], false);
 
-    $('#sortable').sortable('disable');
-  } else if (e.target.id === 'resume') {
-    show(['pause', 'stop'], true);
-    show(['record', 'scan', 'resume'], false);
-    enable(['settings-panel'], false);
+    $("#sortable").sortable("disable");
+  } else if (e.target.id === "pause") {
+    show(["resume", "stop", "pom"], true);
+    show(["record", "scan", "pause"], false);
+    enable(["settings-panel"], false);
 
-    $('#sortable').sortable('disable');
-  } else if ((e.target.id === 'stop') || (e.target.id === 'scan')) {
-    show(['record', 'scan'], true);
-    show(['resume', 'stop', 'pause'], false);
-    enable(['settings-panel'], true);
+    $("#sortable").sortable("disable");
+  } else if (e.target.id === "resume") {
+    show(["pause", "stop","pom"], true);
+    show(["record", "scan", "resume"], false);
+    enable(["settings-panel"], false);
 
-    $('#sortable').sortable('enable');
-  } else if (e.target.id === 'settings') {
-    analytics(['_trackEvent', 'settings', '⚙️']);
-    document.getElementById('settings-panel').classList.toggle('hidden');
+    $("#sortable").sortable("disable");
+  } else if (e.target.id === "stop" || e.target.id === "scan") {
+    show(["record", "scan", "pom"], true); //add pom?
+    show(["resume", "stop", "pause"], false);
+    enable(["settings-panel"], true);
+
+    $("#sortable").sortable("enable");
+  } else if (e.target.id === "pom") {
+    //added so only specific buttons will be available during the POM import
+    // show(["record", "scan", "pom"], true);
+    // show(["resume", "stop", "pause"], false);
+    // enable(["settings-panel"], true);
+  } else if (e.target.id === "settings") {
+    analytics(["_trackEvent", "settings", "⚙️"]);
+    document.getElementById("settings-panel").classList.toggle("hidden");
   }
 
-  if ((e.canSave === false) || (e.target.id === 'record')) {
-    document.getElementById('save').disabled = true;
-  } else if ((e.canSave === true) || (e.target.id === 'scan') || (e.target.id === 'stop')) {
-    document.getElementById('save').disabled = false;
+  if (e.canSave === false || e.target.id === "record") {
+    document.getElementById("save").disabled = true;
+  } else if (
+    e.canSave === true ||
+    e.target.id === "scan" ||
+    e.target.id === "stop"
+  ) {
+    document.getElementById("save").disabled = false;
   }
-  if (e.demo) { document.getElementById('demo').checked = e.demo; }
-  if (e.verify) { document.getElementById('verify').checked = e.verify; }
+  if (e.demo) {
+    document.getElementById("demo").checked = e.demo;
+  }
+  if (e.verify) {
+    document.getElementById("verify").checked = e.verify;
+  }
 }
 
 function busy(e) {
-  if ((e.isBusy === true) || (e.isBusy === false)) {
-    ['scan', 'record', 'stop', 'save', 'save', 'resume'].forEach((id) => {
-      document.getElementById(id).disabled = e.isBusy;
+  if (e.isBusy === true || e.isBusy === false) {
+    ["scan", "record", "stop", "save", "save", "resume"].forEach((id) => {
+      document.getElementById(id).disabled = e.isBusy; //add pom?
     });
   }
 }
 
 function operation(e) {
+  if (e.target.id === "pom") {
+    var popupWindow = window.open(
+      chrome.extension.getURL("./src/background.html"),
+      "exampleName",
+      "width=400,height=400"
+    );
+
+    // var input = document.createElement("input");
+    // input.type = "file";
+
+    // input.onchange = (e) => {
+    //   var file = e.target.files[0];
+    //   console.log(file);
+    //   const name = file.name;
+    //   const size = file.size;
+    //   const type = file.type;
+    //   console.log(name, size, type);
+
+    //   var fr = new FileReader();
+
+    //   var text = fr.result; //text from pom file
+    //   console.log(text);
+
+    //   fr.onload = function (e) {
+    //     var text = fr.result; //text from pom file
+    //     //console.log(text);
+    //     const start = "#robotcorder start";
+    //     const stop = "#robotcorder stop";
+    //     const arr = [];
+    //     while (text.indexOf(stop) !== -1) {
+    //       var mySubString = text.substring(
+    //         text.indexOf(start) + start.length,
+    //         text.indexOf(stop)
+    //       );
+    //       text = text.substring(text.indexOf(stop) + stop.length, text.length);
+    //       //console.log(mySubString.substring(0, mySubString.indexOf("\n")));
+    //       const s = mySubString.split("\n");
+    //       var args_stuff = s[1];
+    //       var just_args = args_stuff
+    //         .substring(args_stuff.indexOf(":") +1, args_stuff.length)
+    //         .split(",");
+
+    //       console.log(just_args);
+    //       var obj_man = {
+    //         keyword: s[2],
+    //         arguments: {
+    //           number: parseInt(just_args[0]),
+    //           types: []
+    //         },
+    //       };
+    //       for (let j=1; j< just_args.length; j++){
+    //         obj_man.arguments.types.push(just_args[j])
+    //       }
+    //       arr.push(obj_man);
+    //       var x = document.getElementById("keywordSelect");
+    //       var option = document.createElement("option");
+    //       option.text = obj_man.keyword;
+    //       option.value = JSON.stringify(obj_man);
+    //       x.add(option);
+    //       /*arr.push(mySubString);
+    //                  console.log(mySubString)
+    //                  console.log("line"); */
+    //     }
+    //     console.log(arr); //adds pom stuff to the console
+    //     //need to get arr to print out within the extension though
+    //   };
+    //   fr.readAsText(file);
+
+    //   //storage.set({ message: text, operation, canSave: false }); */
+    // };
+
+    // input.click();
+  }
   toggle(e);
-  const locators = $('#sortable').sortable('toArray', { attribute: 'id' });
+  const locators = $("#sortable").sortable("toArray", { attribute: "id" });
   host.runtime.sendMessage({ operation: e.target.id, locators }, display);
 
-  analytics(['_trackEvent', e.target.id, '^-^']);
+  analytics(["_trackEvent", e.target.id, "^-^"]);
 }
-
+//some of the button stuff is here
 function settings(e) {
-  const locators = $('#sortable').sortable('toArray', { attribute: 'id' });
-  const demo = document.getElementById('demo').checked;
-  const verify = document.getElementById('verify').checked;
+  const locators = $("#sortable").sortable("toArray", { attribute: "id" });
+  const demo = document.getElementById("demo").checked;
+  const verify = document.getElementById("verify").checked;
   host.runtime.sendMessage({
-    operation: 'settings', locators, demo, verify
+    operation: "settings",
+    locators,
+    demo,
+    verify,
   });
-  analytics(['_trackEvent', 'setting', e.target.id]);
+  analytics(["_trackEvent", "setting", e.target.id]);
 }
 
 function info() {
-  host.runtime.sendMessage({ operation: 'info' });
+  host.runtime.sendMessage({ operation: "info" });
 
-  analytics(['_trackEvent', 'info', 'ℹ️']);
+  analytics(["_trackEvent", "info", "ℹ️"]);
 }
 
 function like() {
-  analytics(['_trackEvent', 'like', '👍']);
+  analytics(["_trackEvent", "like", "👍"]);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  storage.get({
-    message: 'Record or Scan',
-    operation: 'stop',
-    canSave: false,
-    isBusy: false,
-    demo: false,
-    verify: false,
-    locators: []
-  }, (state) => {
-    display({ message: state.message });
-    toggle({
-      target: { id: state.operation },
-      canSave: state.canSave,
-      isBusy: state.isBusy,
-      demo: state.demo,
-      verify: state.verify
+// function pomSave() {
+//   var ta = document.getElementById("tempDiv").getElementsByTagName("textarea");
+//   var activities = document.getElementById("keywordSelect");
+//   var index = activities.selectedIndex;
+//   var arrGuments = [];
+//   arrGuments.push(JSON.parse(activities.options[index].value).keyword);
+//   for (let element of ta) {
+//     arrGuments.push(element.value);
+//   }
+
+//   host.runtime.sendMessage({ operation: "pomer", results: arrGuments });
+// }
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    // chrome.storage.local.get(/* String or Array */ ["pom"], function (items) {
+    //   //  items = [ { "phasersTo": "awesome" } ]
+    //   var arr = JSON.parse(items.pom);
+    //   var x = document.getElementById("keywordSelect");
+
+    //   for (let i = 0; i < arr.length; i++) {
+    //     var option = document.createElement("option");
+    //     option.text = arr[i].keyword;
+    //     option.value = JSON.stringify(arr[i]);
+    //     x.add(option);
+    //   }
+    // });
+    var appendDiv = document.getElementById("keywordDiv");
+
+    //     activities.addEventListener("onchange", function()
+    $("#keywordSelect").change(function (select) {
+      var popupWindow = window.open(
+        chrome.extension.getURL("./src/background.html"),
+        "exampleName",
+        "width=400,height=400"
+      );
+  
+    //   var killDiv = document.getElementById("tempDiv");
+    //   if (killDiv !== null) {
+    //     killDiv.remove();
+    //   }
+    //   var tempDiv = document.createElement("div");
+    //   tempDiv.id = "tempDiv";
+    //   appendDiv.appendChild(tempDiv);
+    //   console.log(select);
+    //   var activities = document.getElementById("keywordSelect");
+
+    //   var index = activities.selectedIndex;
+    //   var reObj = JSON.parse(activities.options[index].value);
+    //   console.log(index, reObj, reObj.arguments.number);
+    //   for (let i = 0; i < reObj.arguments.number; i++) {
+    //     var btn = document.createElement("textarea"); // Create a <button> element
+    //     // btn.innerText = `${reObj.arguments.types[i]}`; // Insert text
+    //     btn.value = `${reObj.arguments.types[i]}`; // Insert text
+    //     if (reObj.arguments.types[i] === "element") {
+    //       btn.addEventListener("click", (eventx) => {
+    //         console.log(eventx)
+    //         document.addEventListener("keydown", (event) => {
+    //           if (event.key === "h") {
+    //             // case sensitive
+
+    //             document.addEventListener("mousemove", recordClickHover, once);
+    //             function recordClickHover(event) {
+    //               btn.value=event.target;
+    //               // const attr = scanner.parseNode(
+    //               //   getTime(),
+    //               //   event.target,
+    //               //   strategyList
+    //               // );
+    //               // attr.type = "hover";
+    //               // if (!handleByChange(attr.type)) {
+    //               //   Object.assign(attr, { trigger: "hover" });
+    //               //   host.runtime.sendMessage({
+    //               //     operation: "action",
+    //               //     script: attr,
+    //               //   });
+    //               // }
+    //             }
+    //           }
+    //         });
+    //       });
+    //     }
+    //     tempDiv.appendChild(btn); // Append <button> to <body>
+    //   }
+    //   var submitButton = document.createElement("input");
+    //   submitButton.type = "button";
+    //   submitButton.value = "submit";
+    //   submitButton.textContent = "submit";
+    //   tempDiv.appendChild(submitButton);
+    //   submitButton.addEventListener("click", pomSave);
     });
-    setTimeout(() => {
-      const sortable = document.getElementById('sortable');
-      state.locators.forEach((locator) => {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(locator));
-        li.setAttribute('id', locator);
-        li.setAttribute('class', 'ui-state-default');
-        sortable.appendChild(li);
-      });
-    }, 200);
-  });
+    storage.get(
+      {
+        message: "Record or Scan",
+        operation: "stop",
+        canSave: false,
+        isBusy: false,
+        demo: false,
+        verify: false,
+        locators: [],
+      },
+      (state) => {
+        
 
-  debug ? document.getElementById('textarea-log').classList.remove('hidden') : 0;
+        display({ message: state.message });
+        toggle({
+          target: { id: state.operation },
+          canSave: state.canSave,
+          isBusy: state.isBusy,
+          demo: state.demo,
+          verify: state.verify,
+        });
+        setTimeout(() => {
+          const sortable = document.getElementById("sortable");
+          state.locators.forEach((locator) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(locator));
+            li.setAttribute("id", locator);
+            li.setAttribute("class", "ui-state-default");
+            sortable.appendChild(li);
+          });
+        }, 200);
+      }
+    );
 
-  ['record', 'resume', 'stop', 'pause', 'save', 'scan'].forEach((id) => {
-    document.getElementById(id).addEventListener('click', operation);
-  });
+    debug
+      ? document.getElementById("textarea-log").classList.remove("hidden")
+      : 0;
 
-  ['demo', 'verify'].forEach((id) => {
-    document.getElementById(id).addEventListener('change', settings);
-  });
+    ["record", "resume", "stop", "pause", "save", "scan", "pom"].forEach(
+      (id) => {
+        //add pom??
+        document.getElementById(id).addEventListener("click", operation);
+      }
+    );
 
-  document.getElementById('like').addEventListener('click', like);
-  document.getElementById('info').addEventListener('click', info);
-  document.getElementById('settings').addEventListener('click', toggle);
+    ["demo", "verify"].forEach((id) => {
+      document.getElementById(id).addEventListener("change", settings);
+    });
 
-  $('#sortable').sortable({ update: settings });
-  $('#sortable').disableSelection();
-}, false);
+    document.getElementById("like").addEventListener("click", like);
+    document.getElementById("info").addEventListener("click", info);
+    document.getElementById("settings").addEventListener("click", toggle);
+
+    $("#sortable").sortable({ update: settings });
+    $("#sortable").disableSelection();
+  },
+  false
+);
 
 host.storage.onChanged.addListener((changes, _) => {
   for (const key in changes) {
-    if (key === 'isBusy') busy({ isBusy: changes.isBusy.newValue });
-    if (key === 'message') display({ message: changes.message.newValue });
+    if (key === "isBusy") busy({ isBusy: changes.isBusy.newValue });
+    if (key === "message") display({ message: changes.message.newValue });
   }
 });
